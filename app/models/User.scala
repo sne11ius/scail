d76f6bcd6e8eb8e6973fbb4235d1474e45f4780c
Cornelius Lilge <cornelius.lilge@gmail.com>
2014-10-03 21:31:13 +0200

    Empty index page is not passing integration tests ;)

---
+++ app/models/User.scala
@@ -0,0 +1,7 @@
+package models
+
+case class User(
+  username: String,
+  passwordHash: String,
+  email: String
+)
--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,24 +1,38 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
 import play.api.test._
 import play.api.test.Helpers._
+import play.Logger
 
-/**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
 @RunWith(classOf[JUnitRunner])
 class IntegrationSpec extends Specification {
 
-  "Application" should {
+  "Index page" should {
 
     "work from within a browser" in new WithBrowser {
+      browser goTo("http://localhost:" + port)
+      browser.pageSource must not be empty
+    }
 
-      browser.goTo("http://localhost:" + port)
-
-      browser.pageSource must contain("Your new application is ready.")
+    "display a username field" in new WithBrowser {
+      browser goTo("http://localhost:" + port)
+      browser.find("input[id='username']") should have length (1)
+    }
+    
+    "display a password field" in new WithBrowser {
+      browser goTo("http://localhost:" + port)
+      browser.find("input[id='password']") should have length (1)
+    }
+    
+    "display a login button" in new WithBrowser {
+      browser goTo("http://localhost:" + port)
+      browser.find("input[id='submit']") should have length (1)
+    }
+    
+    "display a sign-up link" in new WithBrowser {
+      browser goTo("http://localhost:" + port)
+      browser.find("a[id='sign-up']") should have length (1)
     }
   }
 }
